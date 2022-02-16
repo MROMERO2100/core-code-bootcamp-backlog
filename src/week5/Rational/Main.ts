5df57dce9d70ed1bb82d816bdaa1649a816d733c
Moises Abraham Romero Mojica <40069878+MROMERO2100@users.noreply.github.com>
2022-02-16 17:56:45 -0600

    Create Main.ts

---
+++ src/week5/Rational/Main.ts
@@ -0,0 +1,16 @@
+```ts
+import { Rational } from './Rational';
+export class Main {
+  start() {
+    const r1 = new Rational(36, 120);
+    r1.printRational(); // 36 / 120
+    console.log(r1.toFloat()); // 0.3
+    r1.reduce();
+    r1.printRational(); // 3 / 10
+    r1.invert();
+    r1.printRational(); // 10 / 3
+    r1.reduce();
+    r1.printRational(); // 10 / 3
+  }
+}
+```
