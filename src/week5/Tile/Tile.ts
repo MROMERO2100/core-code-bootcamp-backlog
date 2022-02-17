94b769b44921837dbf709ef781bd54cee4c710cb
Moises Abraham Romero Mojica <40069878+MROMERO2100@users.noreply.github.com>
2022-02-16 18:09:12 -0600

    Create Tile.ts

---
+++ src/week5/Tile/Tile.ts
@@ -0,0 +1,18 @@
+export class Tile {
+  letter: string;
+  value: number;
+
+  constructor(letter: string, value: number) {
+    this.letter = letter;
+    this.value = value;
+  }
+
+  printTile() {
+    console.log(`
+        ===========================
+          Letter: ${this.letter}
+          Value: ${this.value}
+        ===========================
+    `);
+  }
+}
