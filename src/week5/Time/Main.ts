7f4a0d4f10c7e0b86cee33de2f32d3bc1f997cf2
Moises Abraham Romero Mojica <40069878+MROMERO2100@users.noreply.github.com>
2022-02-16 18:07:50 -0600

    Create Main.ts

---
+++ src/week5/Time/Main.ts
@@ -0,0 +1,14 @@
+import { Tile } from './Tile';
+export class Main {
+  start() {
+    const A = new Tile('A', 10);
+    A.printTile(); // Example of a reader-friendly format above
+    /*
+      ==================
+        Letter: A
+        Value: 10
+      ==================
+    */
+    const W = new Tile('W', '50'); // This should show and error
+  }
+}
